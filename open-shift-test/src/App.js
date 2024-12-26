928167fb82d072c5c3a48ac7ba3c9002f04c5bd7
amal <amaljw2002@gmail.com>
2024-12-26 22:50:20 +0100

    commit

--- open-shift-test/src/App.js
+++ open-shift-test/src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Version 2.0.0
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
