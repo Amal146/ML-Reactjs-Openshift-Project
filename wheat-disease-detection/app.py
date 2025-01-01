3a1396eafcea224962e9257da596056ec0e2779f
amal <amaljw2002@gmail.com>
2025-01-01 20:51:03 +0100

    renamed:    wheat-disease-detection/Dockerfile -> Dockerfile 	modified:   wheat-disease-detection/app.py 	modified:   wheat-disease-detection/requirements.txt

--- wheat-disease-detection/app.py
+++ wheat-disease-detection/app.py
@@ -60,5 +60,5 @@ def predict():
         return jsonify({'error': f"Internal server error: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
