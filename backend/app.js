565c09d1cd63681d6789e205316240aba1991a0d
amal <amaljw2002@gmail.com>
2025-01-05 03:34:08 +0100

    modified:   backend/app.js 	modified:   backend/package-lock.json 	modified:   backend/package.json

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const indexRouter = require('./routes/index.js'); 
 var cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 
 const app = express();
@@ -11,6 +12,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Define allowed origins (frontend domain)
+const allowedOrigins = [
+    'http://front-maythistime.apps.eu46r.prod.ole.redhat.com'
+];
+// CORS configuration
+app.use(cors({
+    origin: function(origin, callback) {
+        // Allow requests with no origin (e.g., mobile apps, curl requests)
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}));
+
 
 // Use the router for API routes
 app.use('/', indexRouter);
