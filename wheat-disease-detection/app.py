69c0970d02385d83db278b0b3ad6d932a3985481
amal <amaljw2002@gmail.com>
2025-01-06 22:17:17 +0100

    modified:   wheat-disease-detection/app.py

--- wheat-disease-detection/app.py
+++ wheat-disease-detection/app.py
@@ -67,4 +67,4 @@ def predict():
         return jsonify({'error': f"Internal server error: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
